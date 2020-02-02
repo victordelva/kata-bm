672f82b703ff0f028e3008603d7b66ac6b953272
vicalle49@gmail.com <victor.delvalle@koalaboox.com>
2020-02-02 22:30:23 +0100

    change app name

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Victor del Valle</title>
 
         <!-- Fonts -->
         <link href="https://cdn.jsdelivr.net/npm/@mdi/font@4.x/css/materialdesignicons.min.css" rel="stylesheet">
